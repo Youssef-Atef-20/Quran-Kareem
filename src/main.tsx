583bb806f6de9afebcdde83bd98c3b221a539d8e
Youssef Atef <111428670+Youssef-Atef-20@users.noreply.github.com>
2026-02-15 01:45:46 +0200

    Import index.css in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import "./index.css"
 import App from "./App"
 import { QuranProvider } from "./context/QuranProvider"
 import { BrowserRouter } from "react-router-dom"
