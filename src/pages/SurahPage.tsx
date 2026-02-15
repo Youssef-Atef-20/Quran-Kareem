9025967905c7f1f9e3fad796cfe953ef877a915c
Youssef Atef <hh3928850@gmail.com>
2026-02-15 17:33:34 +0200

    e

--- src/pages/SurahPage.tsx
+++ src/pages/SurahPage.tsx
@@ -131,7 +131,7 @@ const SurahPage = () => {
 
       {showSaved && (
         <div className="fixed bottom-8 left-1/2 -translate-x-1/2 bg-green-600 text-white px-6 py-3 rounded-2xl shadow-lg animate-fade flex items-center gap-2 whitespace-nowrap">
-          تم حفظ مكان القراءة ✓
+         تم حفظ مكان آخر آية قرأتها ✓
         </div>
       )}
 
