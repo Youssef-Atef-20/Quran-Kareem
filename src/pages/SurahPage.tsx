19da2d7428cbab6268a3008704c5715b62da8122
Youssef Atef <hh3928850@gmail.com>
2026-02-16 23:17:01 +0200

    d

--- src/pages/SurahPage.tsx
+++ src/pages/SurahPage.tsx
@@ -1,4 +1,4 @@
-import { useParams, Link } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import { useQuran } from "../hooks/useQuran"
 import { surahNames } from "../data/surahNames"
 import { useSettings } from "../hooks/useSettings"
@@ -45,12 +45,12 @@ const SurahPage = () => {
   return (
     <div className="max-w-3xl mx-auto p-6">
 
-      <Link
+      {/* <Link
         to={`/`} replace
         className="fmt-10 mx-auto block w-fit px-6 py-3 rounded-2xl border border-gray-500 text-blue-400 bg-gray-900 hover:bg-gray-200 hover:text-black transition"
       >
        الرجوع للقائمة الرئيسية
-      </Link>
+      </Link> */}
 
       {/* اسم السورة */}
       <h1 className="text-3xl font-bold text-center mb-6 mt-6">
