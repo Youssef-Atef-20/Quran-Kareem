c07c506847d40f016f1bbaa8789bb1eeebc3d4f7
Youssef Atef <hh3928850@gmail.com>
2026-02-16 23:20:00 +0200

    ss

--- src/pages/SurahPage.tsx
+++ src/pages/SurahPage.tsx
@@ -45,12 +45,7 @@ const SurahPage = () => {
   return (
     <div className="max-w-3xl mx-auto p-6">
 
-      {/* <Link
-        to={`/`} replace
-        className="fmt-10 mx-auto block w-fit px-6 py-3 rounded-2xl border border-gray-500 text-blue-400 bg-gray-900 hover:bg-gray-200 hover:text-black transition"
-      >
-       الرجوع للقائمة الرئيسية
-      </Link> */}
+     
 
       {/* اسم السورة */}
       <h1 className="text-3xl font-bold text-center mb-6 mt-6">
