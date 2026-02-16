c0aac4fdbf82be92d7ce4caf460baba28db98024
Youssef Atef <hh3928850@gmail.com>
2026-02-16 23:25:51 +0200

    d

--- src/main.tsx
+++ src/main.tsx
@@ -21,9 +21,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   </React.StrictMode>
 )
 
- if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js");
-  });
-}
+//  if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker.register("/sw.js");
+//   });
+// }
 
